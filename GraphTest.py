f62f652872c88a47f219b68314e28210d11ea023
NissanGoldberg <32920775+NissanGoldberg@users.noreply.github.com>
2018-04-30 20:20:23 +0300

    added graph

---
+++ GraphTest.py
@@ -0,0 +1,25 @@
+from py2neo import Graph,Node,Relationship
+graph = Graph(password="talmud18")
+graph.delete_all()
+
+nicole = Node("Person", name="Nicole", age=24)
+drew = Node("Person", name="Drew", age=20)
+
+mtdew = Node("Drink", name="Mountain Dew", calories=9000)
+cokezero = Node("Drink", name="Coke Zero", calories=0)
+
+coke = Node("Manufacturer", name="Coca Cola")
+pepsi = Node("Manufacturer", name="Pepsi")
+
+graph.create(nicole | drew | mtdew | cokezero | coke | pepsi)
+
+graph.create(Relationship(nicole, "LIKES", cokezero))
+graph.create(Relationship(nicole, "LIKES", mtdew))
+graph.create(Relationship(drew, "LIKES", mtdew))
+graph.create(Relationship(coke, "MAKES", cokezero))
+graph.create(Relationship(pepsi, "MAKES", mtdew))
+graph.create(Relationship(nicole, "KNOWS", drew))
+
+print(graph.run("MATCH (a:Person) RETURN a.name ,a.age").data() )
+
+
