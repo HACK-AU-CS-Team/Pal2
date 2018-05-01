501087411194a8f7225a6ef60fd2ddedc5eaf262
NissanGoldberg <32920775+NissanGoldberg@users.noreply.github.com>
2018-05-01 15:24:46 +0300

    Add files via upload

--- BankTest.py
+++ BankTest.py
@@ -5,13 +5,36 @@ from selenium.webdriver.chrome.options import Options
 chromedriver = "chromedriver"
 driver = webdriver.Chrome(chromedriver)
 
-options = Options()
-options.set_headless(headless=True)
+# options = Options()
+# options.set_headless(headless=True)
+username = "find.freind.184"
+emailLogin = 'yitzhak.sharon7@gmail.com'
+passwordLogin = 'Aa123456'
 
-driver.get('https://e-fibank.bg/EBank/Login')
-browser = webdriver.Chrome(chrome_options=options, executable_path=r'chromedriver')
+# driver.get('https://e-fibank.bg/EBank/Login')
 
-print("Opened Page...")
+# browser = webdriver.Chrome(chrome_options=options, executable_path=r'chromedriver')
+driver = webdriver.Chrome(chromedriver)
+
+#Open this site
+driver.get('https://www.facebook.com/login.php')
+print("Opened facebook...")
+
+# Find element on login page and enter credentials
+email = driver.find_element_by_xpath("//input[@id='email' or @name='email']")
+email.send_keys(emailLogin)
+print("Email Id entered...")
+password = driver.find_element_by_xpath("//input[@id='pass']")
+password.send_keys(passwordLogin)
+print("Password entered...")
+
+button = driver.find_element_by_xpath("//button[@id='loginbutton']")
+button.click()
+print("FB opened")
+
+friend = 'samuel.chemouny.9'
+driver.get('https://www.facebook.com/'+friend+'/friends')
+
 
 html = driver.page_source
 
